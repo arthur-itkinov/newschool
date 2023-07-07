6f5d40ecb534ecf267312a0735bb22904e1b186d
Артур Иткинов <105426684+arthur-itkinov@users.noreply.github.com>
2023-07-07 11:20:38 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -36,7 +36,7 @@ try {
   $mail->Username   = 'itkinov6'; // Логин на почте
   $mail->Password   = 'xvisibffirlmkuni'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
-  $mail->Port       = 465;
+  $mail->Port       = 587;
 
 
   $mail->setFrom('itkinov6@gmail.com', 'Заявка с сайта Новая школа'); // Адрес самой почты и имя отправителя
