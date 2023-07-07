0e49aa8dbd2559ae8bc544cbc10d2d3248be3908
arthur-itkinov <itkinov6@gmail.com>
2023-07-07 11:43:46 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -34,7 +34,7 @@ try {
   // Настройки вашей почты
   $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
   $mail->Username   = 'itkinov6@gmail.com'; // Логин на почте
-  $mail->Password   = 'xvisibffirlmkuni'; // Пароль на почте
+  $mail->Password   = 'pgzorgevsonpjwab'; // Пароль на почте
   $mail->SMTPSecure = 'ssl';
   $mail->Port       = 465;
 
